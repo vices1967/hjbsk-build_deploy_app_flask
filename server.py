dfa979843e51b3f3e9bbfc749eb2569d28bf0890
Victor Viceconti <victor.viceconti@alt3rlab.com>
2024-07-08 11:19:49 -0300

    se agregan errores en las funciones maths

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 # Import the Maths package here
+from Maths.mathematics import summation, subtraction, multiplication
 
 app = Flask("Mathematics Problem Solver")
 
@@ -8,22 +9,29 @@ def sum_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
     # Write your code here
+    result = summation(num1, num2)
+    return str(result)
 
 @app.route("/sub")
 def sub_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
     # Write your code here
+    result = subtraction(num1, num2)
+    return str(result)
 
 @app.route("/mul")
 def mul_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
-    # Write your code here  
+    # Write your code here 
+    result = multiplication(num1, num2)
+    return str(result)
 
 @app.route("/")
 def render_index_page():
     # Write your code here
-    
+    return render_template('index.html')
+   
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
